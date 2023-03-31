971241568746630f113c93c9d1116651fc3085f6
200089_Bee! <200089@virtualwindow.co.za>
2023-03-31 17:28:20 +0200

    db set up

---
+++ models/user.ts
@@ -0,0 +1,11 @@
+import {prop, getModelForClass} from '@typegoose/typegoose';
+
+class User{
+    @prop({required: true})
+    public username?: string;
+
+    @prop({required: true})
+    public password?: string;
+}
+
+export const UserModel = getModelForClass(User);
--- server.ts
+++ server.ts
@@ -2,9 +2,11 @@ import express, {Express} from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
 
 // imported models
 import { InventoryModel } from "./models/inventory";
+import { UserModel } from "./models/user";
 
 // NOTES: 
 // - Nodemon re-runs server as it detects changes
@@ -28,6 +30,52 @@ mongoose.connect(clusterUrl!).then(() => {
     console.log("MongoDB Connected Successfully")
 }).catch((error) => {
     console.log(error.message);
+})
+
+// AUTH Endpoints
+app.post("/user/register", async (req, res) => {
+    
+    try{
+        let {username, password} = req.body
+
+        // hash password
+        password = await bcrypt.hash(password, 10)
+
+        // create user
+        const user = await UserModel.create({username, password})
+
+        res.json(user);
+    } catch(error){
+        res.status(400).json({error});
+    }
+
+})
+
+app.post("/user/login", async (req, res) => {
+    try{
+        // getting variables
+        const {username, password} = req.body
+
+        // checking if user exists
+        const user = await UserModel.findOne({username: username})
+
+        if(user){ // if the user exists
+            //check if the password matches
+            const result = await bcrypt.compare(password, user.password!)
+
+            if (result){ //if the password matches
+                // OPTION: JWT Token
+                res.json({success: true})
+            } else { // if password doesn't match
+                res.status(400).json({error: "Invalid Password"});
+            }
+        } else {
+            res.status(400).json({error: "User does not exist"});
+        }
+
+    } catch(error){
+        res.status(400).json({error});
+    }
 })
 
 // endpoints
